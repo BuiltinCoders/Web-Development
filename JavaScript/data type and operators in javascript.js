1d937480fa62d76f49cbb202fdba37735f8b8f4f
Nirmay <nirmay@gmail.com>
2022-08-09 21:48:50 +0530

    web development

---
+++ JavaScript/data type and operators in javascript.js
@@ -0,0 +1,46 @@
+// variables in javascript
+
+// datatypes - integer
+var a = 12;
+var b = 23;
+var c = 45.01;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// datatype - string
+var fname = "Mohan ";
+console.log(fname);
+var lname = "kumar"
+console.log(lname);
+
+// Operators in javascript
+// there are two types of operators in javascript
+// 1. uniary operator - operator with single operand
+// example:
+c = -32
+console.log(c);
+
+// 2. binary operator - operator with double operand
+// example:
+console.log(23+32);
+
+// Concatenation with string any many other arthimatic opertors in javascript
+console.log(fname + lname);
+
+var num1 = 12;
+var num2 = 2;
+
+console.log("The result of num1+num2 is "+(num1+num2));
+console.log("The result of num1-num2 is "+(num1-num2));
+console.log("The result of num1*num2 is "+(num1*num2));
+console.log("The result of num1/num2 is "+(num1/num2));
+console.log("The result of num1**num2 is "+(num1**num2));
+
+console.log("The result of num++ is " + (num1 ++));
+console.log("The result of num++ is " + (++num1));
+console.log("The result of num-- is " + (num1 --));
+console.log("The result of --num is " + (--num1));
+
+
